a04835400f890843f0e2df7d38b2cabe4f0f52e8
Ed Seymour <eseymour@redhat.com>
2017-05-04 17:30:08 +0100

    simplified the oc module

--- modules/oc.js
+++ modules/oc.js
@@ -3,15 +3,7 @@ module.exports = {
     var exec  = require('child_process').exec,
       child;
 
-    child = exec('/usr/bin/oc ' + cmd, function (error,stdout,stderr) {
-
-      cmdback(error,stdout,stderr)
-
-      console.log('stdout',stdout);
-      console.log('stderr',stderr);
-      if (error != null) {
-        console.log('exec error: ',error);
-      }
-    });
+    child = exec('/usr/bin/oc ' + cmd, cmdback )
+    
   }
 };
