98100b02c92fb9f5e838a8757b1b74053cba20e0
Ed Seymour <eseymour@redhat.com>
2018-03-01 16:14:17 +0000

    complete redo

--- modules/oc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  run_command: function(cmd,cmdback) {
-    var exec  = require('child_process').exec,
-      child;
-
-    child = exec('/usr/bin/oc ' + cmd, cmdback )
-    
-  }
-};
